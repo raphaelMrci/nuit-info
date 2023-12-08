df83488a3e8b3d7695744f847455d94aa2f1f5ca
Raphaël Mercié <raphael.mercie@epitech.eu>
2023-12-08 03:20:08 +0100

    Add info item component

--- front-nuit-info/src/App.js
+++ front-nuit-info/src/App.js
@@ -1,81 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
-
-
+import infoImg from "./assets/motionblur.png";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { InfoItem } from "./components/info/infoComponent.js";
 
 function App() {
-
-  const touchesComponent = (element) => {
-    console.log(element);
-    return(
-      <div className='line'>
-        <div className='notes'>
-        {element.game.map((ele) => {
-          return(
-            <div style={{backgroundColor : 'black', width : 100, height : 50, position : 'absolute', top : ele.y, zIndex : 1}}>
-              <p>hey</p>
+    const touchesComponent = (element) => {
+        console.log(element);
+        return (
+            <div className="line">
+                <div className="notes">
+                    {element.game.map((ele) => {
+                        return (
+                            <div
+                                style={{
+                                    backgroundColor: "black",
+                                    width: 100,
+                                    height: 50,
+                                    position: "absolute",
+                                    top: ele.y,
+                                    zIndex: 1,
+                                }}
+                            >
+                                <p>hey</p>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="touches">
+                    <a
+                        onClick={() => {
+                            console.log("clock");
+                        }}
+                        style={{ color: "white", textAlign: "center" }}
+                    >
+                        {element.id}
+                    </a>
+                </div>
             </div>
+        );
+    };
 
-          );
+    const touches = [
+        { id: 0, game: [] },
+        { id: 1, game: [] },
+        { id: 2, game: [] },
+        { id: 3, game: [] },
+    ];
+    const [touche, settouche] = useState(touches);
+    const [counter, setcounter] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            touche.forEach((element) => {
+                if (element.game.length > 0) {
+                    element.game.forEach((ele) => {
+                        console.log(ele.y);
+                        if (ele.y > 200) {
+                            element.game.splice(ele.id, 1);
+                        }
+                        ele.y += 10;
+                    });
+                }
+            });
+            console.log(touche);
+        }, 500);
+        return () => clearInterval(interval);
+    }, [touche]);
 
-        })}
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const random = Math.floor(Math.random() * 3);
+            touche[random].game.push({
+                id: touche[random].game.length - 1,
+                y: 0,
+            });
+            settouche([...touche]);
+        }, 2000);
+        return () => clearInterval(interval);
+    }, [touche]);
 
+    return (
+        <div className="App">
+            <InfoItem
+                title="Je suis une bonne personne"
+                img={infoImg}
+                alt="logo"
+            />
         </div>
-        <div className = "touches">
-          <a onClick={() => {console.log("clock")}} style={{color : 'white', textAlign : 'center'}}>{element.id}</a>
-        </div>
-      </div>
     );
-
-
-
-  };
-
-
-  const touches = [{id : 0, game : []}, {id : 1, game : []}, {id : 2, game : []}, {id : 3, game : []}]
-  const [touche, settouche] = useState(touches);
-  const [counter, setcounter] = useState(0);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      touche.forEach((element) => {
-        if (element.game.length > 0) {
-         element.game.forEach((ele) => {
-          console.log(ele.y);
-          if (ele.y > 200){
-            element.game.splice(ele.id, 1);
-          }
-            ele.y += 10;
-        });
-        }
-      });
-      console.log(touche);
-    }, 500);
-    return () => clearInterval(interval);
-  }, [touche]);
-
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const random = Math.floor(Math.random() * 3);
-      touche[random].game.push({id : touche[random].game.length - 1, y : 0});
-      settouche([...touche]);
-    }, 2000);
-    return () => clearInterval(interval);
-  }, [touche]);
-
-
-
-  return (
-    <div className="App">
-    <div className="game">
-      <div style={{flexDirection : "row", display : 'flex', justifyContent : 'space-around'}}>
-        <p>{counter}</p>
-      {touche.map(touchesComponent)}
-      </div>
-    </div>
-    </div>
-  );
 }
 
 export default App;

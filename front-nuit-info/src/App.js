0e9f3363c8a38f7863bba22d9dc6c864c4dd768b
Saad Berrada <72012880+Codrux2200@users.noreply.github.com>
2023-12-08 03:55:09 +0100

    [ADD] app not working but waiting for a merge to be able to run

--- front-nuit-info/src/App.js
+++ front-nuit-info/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import {Home} from "./Home/Home";
-
+import { Navbar } from './Navbar/Navbar';
 function App() {
   return (
       <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
+      <Navbar></Navbar>
     </BrowserRouter>
   );
 }
